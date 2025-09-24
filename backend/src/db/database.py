ccbe7615697cd59ac8e10e45bbfdcb4d701e85fc
K Kevin Gladson <kevingladson2427@gmail.com>
2025-09-24 20:50:20 +0530

    dragon feature has been removed

--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -6,3 +6,4 @@ DATABASE_URL = "postgresql://postgres:kevin.2427@localhost/quest_bot_db"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+Base.metadata.create_all(engine)
